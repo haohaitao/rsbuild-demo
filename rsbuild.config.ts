7b3add0558e579d1695ed3bb88fedc9302a7c966
pacino <haitaohhh28@gmail.com>
2024-10-26 13:32:05 +0800

    feat: Perfect the details

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,21 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import git from "git-rev-sync";
 
+const rspack = require("@rspack/core");
+
+process.env.GIT_COMMIT = git.short();
+process.env.GIT_BRANCH = git.branch();
 export default defineConfig({
   plugins: [pluginReact()],
+  tools: {
+    // 与底层工具有关的选项
+    rspack: {
+      plugins: [
+        new rspack.DefinePlugin({
+          "process.env": JSON.stringify(process.env),
+        }),
+      ],
+    },
+  },
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [gitBranch, setGitBranch] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    // 从 Local Storage 中读取 gitBranch
+    const storedGitBranch = localStorage.getItem("gitBranch");
+    if (storedGitBranch) {
+      setGitBranch(storedGitBranch);
+    } else {
+      // 初始化 gitBranch
+      const initialGitBranch = process.env.GIT_BRANCH || "";
+      setGitBranch(initialGitBranch);
+      localStorage.setItem("gitBranch", initialGitBranch);
+    }
+  }, []);
+
+  useEffect(() => {
+    const storedGitBranch = localStorage.getItem("gitBranch");
+    // 当 gitBranch 发生变化时刷新页面
+    if (gitBranch && storedGitBranch !== gitBranch) {
+      setGitBranch(gitBranch);
+      localStorage.setItem("gitBranch", gitBranch);
+      window.location.reload();
+    }
+  }, [gitBranch]);
+
   return (
     <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <h1>我是{gitBranch}分支的需求</h1>
+      <div className="app-env">
+        当前分支: {gitBranch || "Unknown"}
+        <a href="/_branch">切换</a>
+      </div>
     </div>
   );
 };
